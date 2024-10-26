6a54bae1b2227f8566b66a7b44924a441f49f23e
AbdulazizRus <coderdim@gmail.com>
2024-10-26 18:54:39 +0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const registerUser = require('../controller/registerUser');
+const checkEmail = require('../controller/checkEmail');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/email', checkEmail);
 
 module.exports = router;
